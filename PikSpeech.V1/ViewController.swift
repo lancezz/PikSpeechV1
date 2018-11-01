75934adb419ac5c464ff66dc7d1dea08413ce6b5
lance <cielance0@gmail.com>
2018-11-01 10:50:40 -0700

    Initial Commit

---
+++ PikSpeech.V1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PikSpeech.V1
+//
+//  Created by Lance Zhang on 2018-11-01.
+//  Copyright © 2018 cmpt275group11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PikSpeech.V1Tests/PikSpeech_V1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  PikSpeech_V1Tests.swift
+//  PikSpeech.V1Tests
+//
+//  Created by Lance Zhang on 2018-11-01.
+//  Copyright © 2018 cmpt275group11. All rights reserved.
+//
+
+import XCTest
+@testable import PikSpeech_V1
+
+class PikSpeech_V1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
