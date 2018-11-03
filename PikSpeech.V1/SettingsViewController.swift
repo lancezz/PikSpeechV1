7a70483a5de984de7b7d5b8d41ab50e115cfda81
lance <cielance0@gmail.com>
2018-11-02 21:31:07 -0700

    firsty commit

---
+++ PikSpeech.V1/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  PikSpeech.V1
+//
+//  Created by Lance Zhang on 2018-11-02.
+//  Copyright © 2018 cmpt275group11. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PikSpeech.V1/ViewController.swift
+++ PikSpeech.V1/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //all the outlets of the elements
+    @IBAction func settingsButton(_sender: UIButton) {
+        performSegue(withIdentifier: "SettingsSegue", sender: self)
+    }
+    
+    
+    //function viewdidload
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+//        UIDevice.current.setValue(value, forKey:"orientation")
+      
     }
-
-
+ 
+    
+//    Locking to landscapeMode
+//    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+//    return .landscapeLeft
+//    }
+//    override var shouldAutorotate: Bool {
+//        return true
+//    }
 }
+
+
+//////////////////////////
+
 
