8c94d3f6808a7ec262611d0658697fe9033f6b4b
Miguel Taningco <jmigst@gmail.com>
2018-11-04 10:24:21 -0800

    Implemented collection view functions Added Initializer class Edited collectionViewCells attributes in storyboard

---
+++ PikSpeech.V1/Initializer.swift
@@ -0,0 +1,66 @@
+//
+//  Initializer.swift
+//  PikSpeech.V1
+//
+//  Created by Miguel Taningco on 2018-11-04.
+//  Copyright © 2018 cmpt275group11. All rights reserved.
+//
+
+import UIKit
+
+class Initializer: NSObject {
+    
+    static func getCategoryData() -> [TileData]{
+        var categoryDataArray = [TileData]()
+        
+        //append the amount of categories that you want
+        categoryDataArray.append(TileData("bear", "Animals"))
+        categoryDataArray.append(TileData("glasses", "Clothing"))
+        categoryDataArray.append(TileData("Juice", "Drinks"))
+        categoryDataArray.append(TileData("Angry", "Feelings"))
+        categoryDataArray.append(TileData("Bagels", "Food"))
+        
+        return categoryDataArray
+    }
+    
+    static func getCategorySize() -> Int{
+        return 5
+    }
+    
+    static func getAppDataTileData() -> [[TileData]]{
+        let imageName2DArray = [
+            ["bear","cat","chicken","cow","dog","fish","pig","rabbit","squirrel","turtle"],
+            ["glasses","gloves","hat","jacket","shirt","shoes","shorts","socks"],
+            ["Juice","Milk","Soda","Tea"],
+            ["Angry","Cold","confused","disgust","Happy","Hot","hungry","Love","Sad","shocked","Sick","Sleepy"],
+            ["Bagels","burger","carrot","cheese","Chocolate","Eggs","Ice-cream","nuts","pasta","Pizza","potato","Sandwich","vegetable"]
+        ]
+        var appDataTileDataArray = [[TileData]]()
+        
+        print("number of categories: \(getCategorySize())")
+        for i in 0...getCategorySize() - 1{
+            appDataTileDataArray.append([TileData]())
+            print("current size of appTileDataArray: \(appDataTileDataArray.count)")
+            for j in 0...imageName2DArray[i].count - 1{
+                print("size of the 2dArray at index \(i): \(imageName2DArray[i].count)  | j = \(j)")
+                appDataTileDataArray[i].append(TileData(imageName2DArray[i][j], imageName2DArray[i][j]))
+            }
+        }
+        
+        //        for i in 0...getCategorySize(){
+        //            for j in 0...imageName2DArray[i].count{
+        //                appDataTileDataArray[i][j] = TileData(imageName2DArray[i][j], imageName2DArray[i][j])
+        //            }
+        //        }
+        //for each category, for each tile, append that onto the appDataTileData
+        
+        
+        return appDataTileDataArray
+    }
+    
+    static func getDefaultSelectionBarData() -> [TileData]{
+        var appDataTileDataArray = getAppDataTileData()
+        
+        return appDataTileDataArray[0]
+    }
+}
--- PikSpeech.V1/ViewController.swift
+++ PikSpeech.V1/ViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
+    
+    
+    var speechBarTileData = [TileData]()//mutable
+    var selectionBarTileData = Initializer.getDefaultSelectionBarData()//mutable
+    var categoryBarTileData = Initializer.getCategoryData()//immutable.. only carries CATEGORYDATA data
+    var appDataTileData = Initializer.getAppDataTileData()//immutable.. carries all the info of all the pictures
+    
     //all the outlets of the elements
     @IBOutlet weak var selectionCollection: UICollectionView!
     @IBAction func speakButton(_ sender: Any) {
     }
     @IBAction func deletionButton(_ sender: Any) {
     }
-
+    
     @IBOutlet weak var categoryCollection: UICollectionView!
     @IBOutlet weak var sentenceCollection: UICollectionView!
+    
+    /*
+     There are three collection views:
+     selectionCollection - Thats what was supposed to be for the category window, but for now itll just be a holder for our selection pane
+     categoryCollection - Thats supposedly the bar for the categories
+     sentenceCollection - Thats the speech bar
+     */
     
     @IBAction func settingsButton(_sender: UIButton) {
         performSegue(withIdentifier: "SettingsSegue", sender: self)
@@ -28,7 +42,18 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     //function viewdidload
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        //        selectionCollection.register(Tile.self, forCellWithReuseIdentifier: "cellId")
+        selectionCollection.delegate = self
+        selectionCollection.dataSource = self
+        
+        //        categoryCollection.register(Tile.self, forCellWithReuseIdentifier: "cellId")
+        categoryCollection.delegate = self
+        categoryCollection.dataSource = self
+        
+        //        sentenceCollection.register(Tile.self, forCellWithReuseIdentifier: "cellId")
+        sentenceCollection.delegate = self
+        sentenceCollection.dataSource = self
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,24 +62,105 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         //hide Nav Bar
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
     }
- 
+    
+    
+    /*
+     There are three collection views:
+     selectionCollection - Thats what was supposed to be for the category window, but for now itll just be a holder for our selection pane
+     categoryCollection - Thats supposedly the bar for the categories
+     sentenceCollection - Thats the speech bar
+     */
+    
     
     //method numberOfItemsInSection for collectionview
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        if collectionView == self.selectionCollection{
+            //you should return the amount of what is inside your specific collection in the category
+            //this means that this number will differ from time to time
+            return selectionBarTileData.count
+        }
+        else if collectionView == self.categoryCollection{
+            //you should return the amount of categories there are
+            //this should stay still
+            return categoryBarTileData.count
+        }
+        else{
+            //this one changes depending on whats inside the array
+            return speechBarTileData.count
+        }
     }
     
     
     //method cellForItemAt for collectionview
+    //this is what you show in your given collection, basically, you need to make a dummy tile then update it to your liking
+    
+    //dequeue stuff, then return a cell with the specific attributes which is defined by the thing
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        if collectionView == self.selectionCollection{
+            //you should return the amount of what is inside your specific collection in the category
+            //you need to return a tile with the specific thingy of the thingy
+            
+            //when a different category has been chosen, the array gets updated to the latest one
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! Tile
+            cell.setLabel(selectionBarTileData[indexPath.row].getImageTitle())
+            cell.setImage(UIImage(named: selectionBarTileData[indexPath.row].getImageFileName())!)
+            //            cell.setLabel(String(numbers[indexPath.row]))
+            //        cell.labelView.text = String(numbers[indexPath.row])
+            //does this put a cell into the array or takes it away?
+            return cell
+        }
+        else if collectionView == self.categoryCollection{
+            //you should return the amount of categories there are
+            //you should return a tile that contains category info
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! Tile
+            cell.setLabel(categoryBarTileData[indexPath.row].getImageTitle())
+            cell.setImage(UIImage(named: categoryBarTileData[indexPath.row].getImageFileName())!)
+            //            cell.setLabel(String(numbers[indexPath.row]))
+            //        cell.labelView.text = String(numbers[indexPath.row])
+            //does this put a cell into the array or takes it away?
+            return cell
+        }
+        else{
+            //you need to return a tile that is in the speech thing
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! Tile
+            cell.setLabel(speechBarTileData[indexPath.row].getImageTitle())
+            cell.setImage(UIImage(named: speechBarTileData[indexPath.row].getImageFileName())!)
+            //            cell.setLabel(String(numbers[indexPath.row]))
+            //        cell.labelView.text = String(numbers[indexPath.row])
+            //does this put a cell into the array or takes it away?
+            return cell
+        }
     }
     
     
     
     //method did selectitem at for collectionview
+    //THIS IS WHERE THE UI TALKS TO ITESELF AND ITS OBJECTS THIS IS IMPORTANT
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        <#code#>
+        if collectionView == self.selectionCollection{
+            print("selectionCollection selected index \(indexPath.row)")
+            //you should return the amount of what is inside your specific collection in the category
+            //if the selection is pressed, you wanna hand the data over to the speech bar
+            speechBarTileData.append(selectionBarTileData[indexPath.row])
+            //            selectionCollection.reloadData()
+            //            selectionCollection.layoutIfNeeded()
+            
+            //TODO: do we need to update the speech bar?
+        }
+        else if collectionView == self.categoryCollection{
+            //you should return the amount of categories there are
+            //if the selection is pressed, you wanna hand data into the selection bar thing
+            selectionBarTileData = replaceSelectionDataForCategory(indexPath.row)
+            print("categoryCollection selected")
+        }
+        else{
+            //do nothing if youre the speech bar, maybe you can talk if anything
+            print("sentenceCollection selected")
+        }
+    }
+    
+    func replaceSelectionDataForCategory(_ categoryIndex: Int) -> [TileData]{
+        return appDataTileData[categoryIndex]
     }
     
     
@@ -67,6 +173,75 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     //    override var shouldAutorotate: Bool {
     //        return true
     //    }
+}
+
+class TileData{
+    private let imageFileName: String
+    private let imageTitle : String
+    
+    init(_ imageFileName: String, _ imageTitle: String){
+        self.imageFileName = imageFileName
+        self.imageTitle = imageTitle
+    }
+    
+    func getImageFileName() -> String{
+        return imageFileName
+    }
+    
+    func getImageTitle() -> String{
+        return imageTitle
+    }
+}
+
+class Tile: UICollectionViewCell {
+    
+    let imageView : UIImageView = {
+        let imgView = UIImageView()
+        imgView.backgroundColor = .blue
+        imgView.image = UIImage(named: "TeamLogoIcon")
+        return imgView
+    }()
+    
+    let labelView : UILabel = {
+        let labelView = UILabel()
+        labelView.textColor = .red
+        labelView.text = "uilabel"
+        return labelView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    init(_ labelName: String, _ imageName: String, _ rect: CGRect){
+        super.init(frame: rect)
+        labelView.text = labelName
+        imageView.image = UIImage(named: imageName)
+    }
+    
+    func setLabel(_ text: String){
+        labelView.text = text
+    }
+    
+    func setImage(_ image: UIImage){
+        imageView.image = image
+    }
+    
+    
+    func setupViews(){
+        //make the picture and the title
+        addSubview(imageView)
+        addSubview(labelView)
+        //TODO: you need to do the correct anchoring
+        //        imageView.anchorInCorner(.topRight, xPad: self.width*0.01, yPad: self.height*0.01, width: self.width*0.98, height: self.height*0.75)
+        //        labelView.alignAndFill(align: .underCentered, relativeTo: imageView, padding: self.height*0.01)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        //        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+    }
 }
 
 
