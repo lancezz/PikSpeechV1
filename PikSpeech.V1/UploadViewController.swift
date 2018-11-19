b3932e29c361d6724ae790c738abeeae08f8e1c6
lance <cielance0@gmail.com>
2018-11-19 07:39:31 -0800

    now can add photos from library to firebase database and storage. showing up on the colelctionview

--- PikSpeech.V1/UploadViewController.swift
+++ PikSpeech.V1/UploadViewController.swift
@@ -13,6 +13,16 @@ import FirebaseDatabase
 
 class UploadViewController: UIViewController {
     
+    
+    var uploadCategorySegment = "Animals"
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBAction func indexChanged(_ sender: UISegmentedControl) {
+        uploadCategorySegment = sender.titleForSegment(at: sender.selectedSegmentIndex) ?? "Animals"
+        print(uploadCategorySegment)
+        
+    }
+    @IBOutlet weak var uploadTileText: UITextField!
     @IBOutlet weak var uploadImagePreview: UIImageView!
     
     @IBAction func picFromLibrary(_ sender: Any) {
@@ -22,40 +32,69 @@ class UploadViewController: UIViewController {
     }
     
     @IBAction func uploadToFirebase(_ sender: Any) {
-        let storage = Storage.storage()
-        let storageRef = storage.reference()
-        let date = NSDate().timeIntervalSince1970
-        print("Loading the timestamp as the Image name in storage",date)
-        let newUploadRef = storageRef.child("\(date).jpg")
-        //Update the storage on Firebase
-       if let uploadData = uploadImagePreview.image?.jpegData(compressionQuality: 0.1){
-        let uploadTask = newUploadRef.putData(uploadData, metadata: nil, completion: {
-            metadata, error in
-            if error != nil {
+        if uploadTileText != nil {
+            let storage = Storage.storage()
+            let storageRef = storage.reference()
+            let date = NSDate().timeIntervalSince1970
+            print("Loading the timestamp as the Image name in storage",date)
+            let newUploadRef = storageRef.child("\(date).jpg")
+            //Update the storage on Firebase
+            if let uploadData = uploadImagePreview.image?.jpegData(compressionQuality: 0.1){
+                let uploadTask = newUploadRef.putData(uploadData, metadata: nil, completion: {
+                    metadata, error in
+                    if error != nil {
+                        return
+                    }
+                    //print(metadata)
+                    
+                })
+            }
+            //Update the Firebase Database
+            let user = Auth.auth().currentUser
+            guard let uid = user?.uid else
+            {
                 return
             }
-            //print(metadata)
+            var ref: DatabaseReference!
+            ref = Database.database().reference()
+            let userRef = ref.child("user").child(uid)
             
-        })
+            userRef.child("categoryData/\(uploadCategorySegment)/selectionData").observeSingleEvent(of: .value, with:
+                {
+                    (snapshot) in
+                    var value = snapshot.value as? [String] ?? []
+                    value.append("\(self.uploadTileText.text ?? "i")")
+                    userRef.child("categoryData/\(self.uploadCategorySegment)/selectionData").setValue(value)
+                    print(value)
+            }
+            ){
+                (error) in
+                print(error.localizedDescription)
+            }
+            //initialize reference in tileData
+            let uploadTileDataRef = userRef.child("tileData").child("\(self.uploadTileText.text ?? "i")")
+            let uploadTileDataValue = ["Image":"\(date).jpg","frequency":0,"title":"\(self.uploadTileText.text ?? "i")"] as? [String: Any]
+            uploadTileDataRef.setValue(uploadTileDataValue)
+        } else {
+            print("please enter a name for the picture")
         }
-        //Update the Firebase Database
         
         
-    }
+    }//when upload button pressed
     var picker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         picker.delegate = self
-      
+        print(uploadCategorySegment)
         
-    
+        
     }
     
-//    static func uploadImage(_ image: UIImage, at reference: StorageReference, completion: @escaping (?))
-   
- 
-
+    //    static func uploadImage(_ image: UIImage, at reference: StorageReference, completion: @escaping (?))
+    
+    
+    
 }
 
 extension UploadViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
